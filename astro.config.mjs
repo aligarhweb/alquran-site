3c69f36be8e0d0bc00ebbd6a3d29b751dfd280cf
aligarhweb <aligarhweb@gmail.com>
2026-06-28 19:50:00 +0530

    Add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://alquran.in',
+  integrations: [sitemap()]
+});
